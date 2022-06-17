ef5281774c0df474822f428a96ffad5f89603b14
DONGDONG968 <you@example.com>
2022-06-18 04:37:09 +0900

    Update

--- Import Image.py
+++ Import Image.py
@@ -1,5 +1,5 @@
 import cv2
-img = cv2.imread('image/cat.jpg')
-cv2.imshow('CAT',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+img = cv2.imread('image/cat.jpg') # 해당 경로의 파일(이미지) 읽어 오기
+cv2.imshow('CAT',img) # img 라는 이름의 창에 CAT 을 표시
+cv2.waitKey(0) # 지정된 시간(ms) 동안 사용자 키 입력 대기
+cv2.destroyAllWindows() # 모든 창 닫기
