7861be47858b4a53d07de4bb2e6cb8909d716d9d
DONGDONG968 <you@example.com>
2022-07-04 23:25:43 +0900

    얼굴을 인식하여 캐릭터 씌우기 -1

---
+++ 10. face detection vs face recognition.py
@@ -0,0 +1,32 @@
+import cv2
+import mediapipe as mp
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+
+# For webcam input:
+cap = cv2.VideoCapture('face_video.mp4')
+with mp_face_detection.FaceDetection(
+    model_selection=0, min_detection_confidence=0.5) as face_detection:
+    while cap.isOpened():
+      success, image = cap.read()
+      if not success:
+        break
+
+      # To improve performance, optionally mark the image as not writeable to
+      # pass by reference.
+      image.flags.writeable = False
+      image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+      results = face_detection.process(image)
+
+      # Draw the face detection annotations on the image.
+      image.flags.writeable = True
+      image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+      if results.detections:
+        for detection in results.detections:
+          mp_drawing.draw_detection(image, detection)
+      # Flip the image horizontally for a selfie-view display.
+      cv2.imshow('MediaPipe Face Detection', image)
+      if cv2.waitKey(1) == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
